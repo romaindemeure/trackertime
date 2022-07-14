9396e62426e3d921f1735d1043c2256ac6a6e2db
Demeure Romain <demeureromain4@gmail.com>
2022-07-14 12:10:55 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="main">
+    <img :src="source" alt="vue3 image" class="logo">
+    <h1>{{ title }}</h1>
+    <h2>Nom de la tâche en cours : {{ taskName }}</h2>
+    <h3>Tache en cours {{ (isTaskInProgress) ? 'oui' : 'non' }}</h3>
+    
+    <button @click="startTask()" class="button">Démarrer</button>
+    <button @click="stopTask" class="button">Arrêter</button>
+
+    <div class="error">
+      <p>{{ errorMsg }}</p>
+    </div>
+
+    <div>
+      <input type="text" v-model="taskName">
+    </div>
+
+    <table>
+      <thead>
+        <tr>
+          <th>id</th>
+          <th>Tâche</th>
+          <th>Début</th>
+          <th>Fin</th>
+          <th>Durée</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="task in tasks">
+          <th>{{ task.id }}</th>
+          <th>{{ task.name }}</th>
+          <th>{{ formatTimestramp(task.start) }}</th>
+          <th>{{ formatTimestramp(task.end) }}</th>
+          <th>{{ durationBetweenTimestamps(task.start, task.end) }}</th>
+        </tr>
+      </tbody>
+    </table>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Hello World',
+      source: 'https://cdn.svgporn.com/logos/vue.svg',
+      tsFormatter: Intl.DateTimeFormat('fr', {hour: '2-digit', minute: '2-digit'}),
+      tasks: [],
+      taskID: 0,
+      taskName: '',
+      isTaskInProgress: false,
+      startTime: null,
+      errorMsg: null
+    }
+  },
+  methods: {
+    startTask () {
+      // Vérifications
+      if (this.taskName.length == 0) {
+        this.errorMsg = "Le nom d'une tâche ne peut pas être vide"
+        return
+      } else if (this.isTaskInProgress) {
+        this.errorMsg = "une tâche est déjà en cours"
+        return
+      } else {
+        this.errorMsg = null
+      }
+
+      // Début de la tâche
+      this.isTaskInProgress = true
+      this.startTime = Date.now()
+    },
+    stopTask () {
+      // Vérifications
+      if (!this.isTaskInProgress) {
+        this.errorMsg = "Aucune tâche n'est en cours"
+        return
+      }
+
+      // Enregistrement de la tâche
+      this.tasks.unshift({
+        id: this.getAnID(),
+        name: this.taskName,
+        start: this.startTime,
+        end: Date.now()
+      })
+
+      // Fin de la tâche
+      this.isTaskInProgress = false
+      this.errorMsg = null
+      this.taskName = ''
+    },
+    getAnID() {
+      this.taskID++
+      return this.taskID
+    },
+    formatTimestramp(ts) {
+      return this.tsFormatter.format(ts)
+    },
+    durationBetweenTimestamps (start, end) {
+      let seconds = Math.floor((end / 1000) - (start / 1000))
+      let minutes = Math.floor(seconds / 60)
+      const hours = Math.floor(minutes / 60)
+      seconds = seconds % 60
+      minutes = minutes % 60
+      return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`
+    }
+  },
+}
+</script>
+
+<style scoped>
+.main {
+  text-align: center;
+}
+.error {
+  background: red;
+}
+
+.button {
+  margin: 5px;
+}
+</style>
