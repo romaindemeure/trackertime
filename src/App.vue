be79d6607d2093c91c3949195a20245c7273c7d8
Demeure Romain <demeureromain4@gmail.com>
2022-07-17 00:10:00 +0200

    application works

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,10 @@
     <img :src="source" alt="vue3 image" class="logo">
     <h1>{{ title }}</h1>
     <h2>Nom de la tâche en cours : {{ taskName }}</h2>
-    <h3>Tache en cours {{ (isTaskInProgress) ? 'oui' : 'non' }}</h3>
+    <h3>Tache en cours : {{ (isTaskInProgress) ? 'oui' : 'non' }}</h3>
+    <h3>Durée en cours : {{ currentDuration }}</h3>
     
-    <button @click="startTask()" class="button">Démarrer</button>
-    <button @click="stopTask" class="button">Arrêter</button>
+    <button @click="toggleTask" class="button" :class="isTaskInProgress ? 'red' : 'blue'">{{ isTaskInProgress ? 'Arrêter' : 'Démarer'}}</button>
 
     <div class="error">
       <p>{{ errorMsg }}</p>
@@ -53,7 +53,30 @@ export default {
       taskName: '',
       isTaskInProgress: false,
       startTime: null,
+      nowTime: null,
+      internalEverySecond: null,
       errorMsg: null
+    }
+  },
+  computed: {
+    currentDuration () {
+      if (this.startTime && this.nowTime) {
+        return this.durationBetweenTimestamps(this.startTime, this.nowTime)
+      } else {
+        return '00:00:00'
+      }
+      
+    }
+  },
+  watch: {
+    isTaskInProgress(isInProgress) {
+      if (isInProgress) {
+        this.internalEverySecond = setInterval(() => {
+          this.nowTime = Date.now()
+        }, 1000)
+      } else {
+        clearInterval(this.internalEverySecond)
+      }
     }
   },
   methods: {
@@ -72,6 +95,7 @@ export default {
       // Début de la tâche
       this.isTaskInProgress = true
       this.startTime = Date.now()
+      this.nowTime = Date.now()
     },
     stopTask () {
       // Vérifications
@@ -89,9 +113,18 @@ export default {
       })
 
       // Fin de la tâche
+      clearInterval(this.internalEverySecond)
       this.isTaskInProgress = false
       this.errorMsg = null
+      this.nowTime = null
       this.taskName = ''
+    },
+    toggleTask() {
+      if (this.isTaskInProgress) {
+        this.stopTask()
+      } else {
+        this.startTask()
+      }
     },
     getAnID() {
       this.taskID++
@@ -101,6 +134,7 @@ export default {
       return this.tsFormatter.format(ts)
     },
     durationBetweenTimestamps (start, end) {
+      console.log(start, end)
       let seconds = Math.floor((end / 1000) - (start / 1000))
       let minutes = Math.floor(seconds / 60)
       const hours = Math.floor(minutes / 60)
@@ -120,6 +154,21 @@ export default {
   background: red;
 }
 
+.red {
+  background: red;
+  color: white;
+  border: none;
+  padding: 10px;
+  border-radius: 5px;
+  cursor: pointer;
+}
+.blue {
+  background: lightblue;
+  border: none;
+  padding: 10px;
+  border-radius: 5px;
+  cursor: pointer;
+}
 .button {
   margin: 5px;
 }
